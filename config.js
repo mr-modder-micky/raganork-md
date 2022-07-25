e388534f84da0128cc0ec7ba7cd161130c241140
mr-modder-micky <105913371+mr-modder-micky@users.noreply.github.com>
2022-07-25 10:20:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ module.exports = {
     IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f","deb80cd12ababea1c9b9a8ad6ce3fab2","78c84c62b32a88e86daf87dd509a657a"],
     RG: process.env.RG || '919074309534-1632403322@g.us',
     PASSWORD: process.env.PASSWORD || '',
-    BOT_INFO: process.env.BOT_INFO || 'Raganork;Souravkl11;916282344739;https://i.imgur.com/st4R2dq.jpeg;https://chat.whatsapp.com/Dt3C4wrQmt0GG6io1IBIHb',
+    BOT_INFO: process.env.BOT_INFO || 'MICKY MWON BOT;MICKY SER;919526123594;https://i.imgur.com/R9bKlqN.jpeg;https://chat.whatsapp.com/DgNHqhJd07M3plApO4NfU7',
     RBG_KEY: process.env.RBG_KEY || '',
     ALLOWED: process.env.ALLOWED || '91,94,212',
     CHATBOT: process.env.CHATBOT || 'off',
